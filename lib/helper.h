1a255bf58ad46fad166137c603a2af99a61ccdf7
Daniel Veiga <daveigantu@gmail.com>
2020-03-06 17:08:25 -0300

    PrintTable Add

--- lib/helper.h
+++ lib/helper.h
@@ -1,6 +1,19 @@
 #ifndef _HELPER_LIB_
 #define _HELPER_LIB_
 
-int * RandomVector(int size);
+typedef struct Table
+{
+	char type;
+	char title[30];
+    struct Cell
+    {
+    	int intContent;
+    	char charContent[30];
+    } row[30];
+} Table;
+
+void InsertRandomVector(int * vector, int size);
+void PrintTable(Table table[], int row, int col);
+void Int2Str(int num, char * str);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <helper.h>
+
 
 int main()
 {
-	printf("Ola Mundo\n");
+	printf("---Algoritmos de Ordenação---\n\n");
+	printf("Tamanho do Vetor: ");
+	
+	int num;
+	scanf("%d", &num);
+	
+	int vetor[num];
+	InsertRandomVector(vetor,num);
+
+	Table table[num];
+
+	for (int i = 0; i < num; i++)
+		Int2Str(i, table[i].title);
+
+	for (int i = 0; i < num; i++)
+		Int2Str(vetor[i], table[i].row[0].charContent);
+
+	PrintTable(table, 1, num);
+
 	return 0;
 }
